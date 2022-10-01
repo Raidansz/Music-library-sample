32a5272757c911c3c20a2756649659eedb9aa8f3
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-01 17:20:28 +0200

    Add project files.

---
+++ ATWSMF_ADT_2022_23_1.Endpoint/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ATWSMF_ADT_2022_23_1.Endpoint
+{
+    public class Class1
+    {
+    }
+}
---
+++ ATWSMF_ADT_2022_23_1.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ATWSMF_ADT_2022_23_1.Logic
+{
+    public class Class1
+    {
+    }
+}
