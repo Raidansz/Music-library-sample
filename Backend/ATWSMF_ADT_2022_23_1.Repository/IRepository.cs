803fd6f81e68fbf42dce4fc5aa98678e68310c98
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-31 20:04:00 +0100

    Created Repository class + IRepository interface

---
+++ Backend/ATWSMF_ADT_2022_23_1.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATWSMF_ADT_2022_23_1.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetOne(int id);
+        void Update(int id, T entity);
+        void AddNew(T entity);
+        void Delete(T entity);
+
+    }
+}
---
+++ Backend/ATWSMF_ADT_2022_23_1.Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATWSMF_ADT_2022_23_1.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        public void AddNew(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
