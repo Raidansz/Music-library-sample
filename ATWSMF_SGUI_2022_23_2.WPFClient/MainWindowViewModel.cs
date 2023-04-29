2aaeabcc034f1f857983dd920dbebc8e18f9d875
Raidansz <92235772+Raidansz@users.noreply.github.com>
2023-04-29 13:45:16 +0200

    Adding song dialog

---
+++ ATWSMF_SGUI_2022_23_2.WPFClient/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ATWSMF_SGUI_2022_23_2.WPFClient
+{
+    internal class MainWindowViewModel
+    {
+        public ICommand Songs { get; set; }
+
+
+
+
+
+        public MainWindowViewModel()
+        {
+            Songs = new RelayCommand(() =>
+            {
+                Songs window = new Songs();
+                window.Show();
+            });
+        }
+    }
+}
