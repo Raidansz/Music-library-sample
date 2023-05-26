a6dd2508861c890fe6ab91fbdb8eecd3ce10f35f
raidan <raidan@DESKTOP-1L18PEQ>
2023-05-26 18:20:55 +0200

    Adding a landing page.

--- ATWSMF_SGUI_2022_23_2.JSWebClient/server.js
+++ ATWSMF_SGUI_2022_23_2.JSWebClient/server.js
@@ -18,8 +18,38 @@ const server = http.createServer(function (req, res) {
     }
 
     // Serve static files
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'Album.html'), 'utf8', function (err, content) {
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile(path.join(__dirname, 'index.html'), 'utf8', function (err, content) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (req.url === '/song.html') {
+        fs.readFile(path.join(__dirname, 'song.html'), 'utf8', function (err, content) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (req.url === '/album.html') {
+        fs.readFile(path.join(__dirname, 'album.html'), 'utf8', function (err, content) {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else if (req.url === '/artists.html') {
+        fs.readFile(path.join(__dirname, 'artists.html'), 'utf8', function (err, content) {
             if (err) {
                 res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Internal Server Error');
@@ -46,6 +76,8 @@ const server = http.createServer(function (req, res) {
             }
         });
     }
+
+
 });
 
 server.listen(port, function () {
